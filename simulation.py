a5b9ff6c21503202239730da21dca0be40cad250
Norbert <54719382+NorbertCode@users.noreply.github.com>
2024-12-09 19:38:44 +0100

    stylistic changes

--- simulation.py
+++ simulation.py
@@ -14,18 +14,22 @@ class Simulation:
         output = Image.new("RGB", self._resolution)
         draw_output = ImageDraw.Draw(output)
 
-        image_center = (self._resolution[0] // 2, self._resolution[1] // 2)
+        image_center = (self._resolution[0] / 2, self._resolution[1] / 2)
         pixel_radius = (self._center_object.diameter / 2) / self._meters_per_pixel
         center_obj_fill_color = (255, 255, 255)
         draw_output.circle(image_center, pixel_radius, fill=center_obj_fill_color)
 
         point_obj_fill_color = (0, 255, 0)
         for obj in point_objects:
-            obj_position = (obj.position[0] / self._meters_per_pixel, obj.position[1] / self._meters_per_pixel)
+            obj_position = (obj.position[0] / self._meters_per_pixel,
+                             obj.position[1] / self._meters_per_pixel)
             draw_output.point(obj_position, point_obj_fill_color)
 
         output.show()
 
 
-sim = Simulation((128, 128), 10, center_object.CenterObject(100, 0), [point_object.PointObject((10, 10), 0, 0)])
+test_objs = [point_object.PointObject((200, 200), 100, None),
+             point_object.PointObject((750, 750), 500, None),
+             point_object.PointObject((200, 750), 1000, None)]
+sim = Simulation((128, 128), 10, center_object.CenterObject(100, 0), test_objs)
 sim.draw(sim._center_object, sim._point_objects)
