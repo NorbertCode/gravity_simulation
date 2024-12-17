5915d5ef5d5ad7b6ac9d82af91eec1cbb314f0f0
Norbert <54719382+NorbertCode@users.noreply.github.com>
2024-12-18 00:35:12 +0100

    aesthetic changes

--- simulation.py
+++ simulation.py
@@ -21,7 +21,7 @@ class Simulation:
         self._center_obj.set_position(np.array(image_center) * self._meters_per_pixel)
 
     def calculate_next(self, point_object: PointObject) -> PointObject:
-        next_point = deepcopy(point_object)  # So it creates a copy instead of changing the param
+        next_point = deepcopy(point_object)  # Create copy instead of changing param
 
         dist_vector = np.array([self._center_obj.position[0] - next_point.position[0],
                            self._center_obj.position[1] - next_point.position[1]])
@@ -36,12 +36,12 @@ class Simulation:
         next_point.set_velocity(next_point.velocity + (accel_vector * self.TIME_STEP))
         return next_point
 
-    def run_simulation_for_obj(self, steps: int, point_object: PointObject) -> list[PointObject]:
+    def run_simulation_for_obj(self, steps: int,
+                               point_object: PointObject) -> list[PointObject]:
         point_obj_steps_list = [point_object]
         for step in range(steps):
             point_obj_steps_list.append(self.calculate_next(point_obj_steps_list[step]))
         return point_obj_steps_list
-        # TODO: Maybe return list of positions instead of PointObjects?
 
     def draw(self, center_object: CenterObject, point_objects: list[PointObject]):
         output = Image.new("RGB", self._resolution)
@@ -63,6 +63,7 @@ class Simulation:
 
 
 # 55000 as meters per pixel so the image spans 7000000 pixels in each direction
-test_objs = [PointObject(np.array([128 * 55000., (128 * 55000) - 6771000.]), 1000, np.array([7672, 0]))]
+test_objs = [PointObject(np.array([128 * 55000., (128 * 55000) - 6771000.]),
+                         1000, np.array([7672, 0]))]
 sim = Simulation((256, 256), 55000, CenterObject(6731000., 5.972e24), test_objs)
 sim.draw(sim._center_obj, sim._point_objs)
